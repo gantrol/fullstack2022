20708d036f450e68ae9e245539cb3bd065c478dd
gantrol <gantrol.h@qq.com>
2022-07-17 10:25:31 +0800

    1.7: unicafe step2

--- part1/unicafe/src/Statistic.js
+++ part1/unicafe/src/Statistic.js
@@ -1,10 +1,17 @@
 export const Statistics = ({ good, nentral, bad }) => {
+  const sum = good + nentral + bad
+  const point = good - bad
+  const average = point / sum
+  const positive =  good / sum
   return (
     <>
       <h3>statistics</h3>
       <p>good {good}</p>
       <p>neutral {nentral}</p>
       <p>bad {bad}</p>
+      <p>all {sum}</p>
+      <p>average {average}</p>
+      <p>positive {positive * 100} %</p>
     </>
   )
 }
