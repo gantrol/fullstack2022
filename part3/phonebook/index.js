4753977e0c10368907b41cb396d0d4bb9739c862
gantrol <501963347@qq.com>
2022-07-29 17:30:53 +0800

    3.6: Phonebook backend step6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
 const persons = [
@@ -40,10 +41,34 @@ app.get('/api/persons', (req, res) => {
   res.send(persons)
 })
 
+app.post('/api/persons', (req, res) => {
+  // TODO：校验逻辑
+  const id = Math.ceil(Math.random() * 9999999);
+  const person = req.body
+  console.log(person)
+
+  if (person.name == '' || person.number == '') {
+    return res.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  // name is duplicative
+  if (persons.find(p => p.name === person.name)) {
+    return res.status(400).json({ 
+      error: `${person.name} is duplicative`
+    })
+  }
+  const personObject = { ...person, id }
+  persons.push(personObject)
+  res.json(personObject)
+})
+
 app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id
-  if (id <= persons.length && id > 0) {
-    res.send(persons[id - 1])
+  const index = persons.findIndex(p => p.id === id)
+  if (index > -1) {
+    res.send(persons[index])
   } else {
     res.status(404).send("Sorry can't find that!")
   }
