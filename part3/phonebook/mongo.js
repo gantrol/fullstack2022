199ed5644d95cfeff2604cc0ff82ef8c81f784a0
gantrol <gantrol.h@qq.com>
2022-07-30 23:04:40 +0800

    3.12: Command-line database

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 5) {
+  console.log('Usage: node mongo.js <account> <password> <name> <phonenumber>')
+  process.exit(1)
+}
+
+const account = process.argv[2]
+const password = process.argv[3]
+const name = process.argv[4]
+const phoneNumber = process.argv[5]
+
+const url = `mongodb+srv://${account}:${password}@cluster0.hrs3q.mongodb.net/?retryWrites=true&w=majority`
+const phoneBookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const PhoneBook = mongoose.model('PhoneBook', phoneBookSchema)
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected')
+
+    const phonebook = new PhoneBook({
+      name: name,
+      number: phoneNumber,
+    })
+
+    return phonebook.save()
+  })
+  .then(() => {
+    console.log('phonenumber saved!')
+    return mongoose.connection.close()
+  })
+  .catch((err) => console.log(err))
